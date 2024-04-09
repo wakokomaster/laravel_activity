ae5250efb547b08e4a6f858c2a46cc79fd7da8e3
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-04-09 09:06:10 +0800

    implemented auth logic

--- routes/web.php
+++ routes/web.php
@@ -19,27 +19,39 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::controller(GenderController::class)->group(function () {
-    Route::get('/gender/create', 'create');
-    Route::get('/gender/show/{id}', 'show');
-    Route::get('/gender', 'index');
-    Route::get('/gender/edit/{id}', 'edit');
-    Route::get('/gender/delete/{id}', 'delete');
-
-    Route::post('/gender/store', 'store');
-    Route::put('/gender/update/{gender}', 'update');
-    Route::delete('/gender/destroy/{gender}', 'destroy');
+Route::group(['middleware' => 'guest'], function () {
+    // NOTE: ->name() is some magical shiiiieet (I don't like it)
+    Route::get('/login', [UserController::class, 'login'])->name('login');
+    Route::post('/login', [UserController::class, 'loginAuth']);
 });
 
-Route::controller(UserController::class)->group(function () {
-    Route::get('/user/create','create');
-    Route::get('/user','index');
-    Route::get('/user/show/{id}','show');
-    Route::get('/user/edit/{id}','edit');
-    Route::get('/user/delete/{id}','delete');
 
+Route::group(['middleware' => 'auth'], function () {
+    Route::controller(GenderController::class)->group(function () {
+        Route::get('/gender/create', 'create');
+        Route::get('/gender/show/{id}', 'show');
+        Route::get('/gender', 'index');
+        Route::get('/gender/edit/{id}', 'edit');
+        Route::get('/gender/delete/{id}', 'delete');
 
-    Route::post('/user/store','store');
-    Route::put('/user/update/{user}','update');
-    Route::delete('/user/destroy/{user}','destroy');
+        Route::post('/gender/store', 'store');
+        Route::put('/gender/update/{gender}', 'update');
+        Route::delete('/gender/destroy/{gender}', 'destroy');
+    });
+
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('/home', 'index');
+        Route::get('/user', 'index');
+        Route::get('/user/create', 'create');
+        Route::get('/user/show/{id}', 'show');
+        Route::get('/user/edit/{id}', 'edit');
+        Route::get('/user/delete/{id}', 'delete');
+
+        Route::get('/logout', [UserController::class, 'logout']);
+
+        Route::post('/user/store', 'store');
+        Route::put('/user/update/{user}', 'update');
+        Route::delete('/user/destroy/{user}', 'destroy');
+    });
 });
