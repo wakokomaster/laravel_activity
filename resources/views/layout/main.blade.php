8c4ec5f93fb24d5c21a2ae05a51e65425fa4bef3
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-04-09 09:01:05 +0800

    fixed styles

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,14 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="{{ asset  ('/css/bootstrap.min.css') }}">
-    
+
+    <title>Document</title>
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+        }
+
+        body {
+            height: 100vh;
+        }
+    </style>
 </head>
+
 <body>
     @yield('content')
     <script src="{{asset('/js/bootstrap.bundle.min.js')}}"></script>
 </body>
+
 </html>
