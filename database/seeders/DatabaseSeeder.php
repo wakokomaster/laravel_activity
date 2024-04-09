cae12a81fc8684885508b1d910fad8ba860ca351
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-04-09 09:02:33 +0800

    seed more users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,6 +27,6 @@ class DatabaseSeeder extends Seeder
             'gender' => 'Female',
         ]);
 
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(100)->create();
     }
 }
